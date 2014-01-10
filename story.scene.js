ddf1aca42efe11f47006f365481bbd2e8f9039d5
yfwz100 <yfwz100@yeah.net>
2014-01-10 21:10:06 +0800

    Change to simple arrary implementation.

--- story.scene.js
+++ story.scene.js
@@ -8,7 +8,7 @@
     if (exports.ziq && exports.ziq.Story) {
         namespace = exports.ziq.Story;
     } else {
-        throw 'ziq.Story is needed.';
+        throw 'ziq.Story is required.';
     }
     
     /** Check if the two rectangle area is intersected. */
@@ -20,29 +20,28 @@
      * Create a new Scene function.
      * @constructor
      * @function
-     * @param {object} ctx - the context of the scene.
+     * @param ctx the context of the scene.
+     * @param callback (optional) the update callback function.
      */
-    function Scene(ctx) {
-        var actors = {};
+    function Scene(ctx, callback) {
+        var actors = [];
         
         /**
          * The implementation of scene function.
          * @function
-         * @param {Number} frame - current frame.
+         * @param {Number} frame current frame.
          */
         function scene(frame) {
             // Traverse the actors and update them.
-            var keys = Object.keys(actors), i, actor;
-            for (i = 0; i < keys.length; i += 1) {
-                actor = actors[keys[i]].object;
-                
-                if (actor.update) {
-                    actor.update(frame);
-                }
-                
-                if (actor.paint) {
-                    actor.paint(ctx);
-                }
+            var i, actor;
+            ctx.save();
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            ctx.restore();
+            for (i = 0; i < actors.length; i += 1) {
+                actors[i].object(ctx);
+            }
+            if (callback) {
+                callback(frame);
             }
         }
         
@@ -63,11 +62,10 @@
             if (!actor) {
                 actor = rect;
             }
-            var pos = [rect.x, rect.y, rect.w, rect.h];
-            actors[pos] = {
+            actors.push({
                 rect: rect,
                 object: actor
-            };
+            });
             actor.scene = scene;
         };
         
@@ -76,23 +74,23 @@
          * @param {function} callback - the callback(rect, object) function
          */
         scene.forEach = function (callback) {
-            var keys = Object.keys(actors), i;
-            for (i = 0; i < keys.length; i += 1) {
-                callback(actors[keys[i]].rect, actors[keys[i]].object);
+            var i;
+            for (i = 0; i < actors.length; i += 1) {
+                callback(actors[i].rect, actors[i].object);
             }
         };
         
         /**
          * Remove an actor. If the second parameter is absent, all objects within that area will be cleared.
-         * @param {object} rect - rectangle area of the actor.
-         * @param {object} actor - (optional) the actor.
+         * @param rect rectangle area of the actor.
+         * @param actor (optional) the actor.
          */
         scene.removeActor = function (rect, actor) {
-            var keys = Object.keys(actors), i, a, b;
-            for (i = 0; i < keys.length; i += 1) {
-                if (isRectsInterseted(actors[keys[i]].rect, rect)) {
-                    if (!actor || actor === actors[keys[i]].object) {
-                        delete actors[keys[i]];
+            var i, a, b;
+            for (i = 0; i < actors.length; i += 1) {
+                if (isRectsInterseted(actors[i].rect, rect)) {
+                    if (!actor || actor === actors[i].object) {
+                        delete actors[i];
                     }
                 }
             }
@@ -100,8 +98,8 @@
         
         /**
          * Get an arrary of actors around the selected area.
-         * @param {object} rect - the rectangle area(x,y,w,h).
-         * @returns {array}
+         * @param rect the rectangle area(x,y,w,h).
+         * @returns {Array}
          */
         scene.getActors = function (rect) {
             var keys = Object.keys(actors), i, results = [];
